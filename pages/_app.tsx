9989c63b25d9d93897fa8b07a912f63172dc1d03
zhangzhen <zhangzhen@zhangzhens-MacBook-Pro.local>
2023-10-01 22:35:55 +0800

    chore: bump versions and use TS

---
+++ pages/_app.tsx
@@ -0,0 +1,57 @@
+import 'nextra-theme-blog/style.css'
+import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
+
+import '../styles/main.css'
+
+export default function Nextra({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/Inter-roman.latin.var.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        {/* <link rel="manifest" href="/site.webmanifest" /> */}
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS"
+          href="/feed.xml"
+        />
+
+        {/* TODO use local js to avoid network error, see if can be improved*/}
+      </Head>
+        <Script
+          src="./scripts/linkedin-badge.js"
+          // src="https://platform.linkedin.com/badges/js/profile.js"
+          async
+          defer
+          type="text/javascript"
+        />
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
+}
